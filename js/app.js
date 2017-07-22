0f3f808773b8273a6b4a9dea7bfcac45f18aaa0c
Yewhral <zabrzyjewski@gmail.com>
2017-07-22 15:40:10 +0200

    data binding with v-htm and v-bind

--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@ new Vue({
     el:'#vue-app',
     data:{
         name:'Michal',
-        job:'JS dev'
+        job:'JS dev',
+        website:'http//www.thenetninja.co.uk',
+        websiteTag: '<a href="http//www.thenetninja.co.uk">Test again</a>'
     },
+
     methods:{
         greet:function(time) {
             return`Good ${time} ${this.name}`;
