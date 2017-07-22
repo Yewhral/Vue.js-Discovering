ac6ef214e48f610ba1d4199d9b28355517f0467f
Yewhral <zabrzyjewski@gmail.com>
2017-07-22 14:21:37 +0200

    Vue data and methods

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 new Vue({
     el:'#vue-app',
     data:{
-        name:'Michal'
+        name:'Michal',
+        job:'JS dev'
+    },
+    methods:{
+        greet:function(time) {
+            return`Good ${time} ${this.name}`;
+        }
     }
 });
